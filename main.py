55dfcd8c9e69b4e472acb04804cf14f90be8eb29
AvaAvarai <alice.williams.tech@gmail.com>
2025-09-29 12:24:21 -0700

    first plotting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pdbreader
 import pandas as pd
+import matplotlib.pyplot as plt
 
 DATA_FOLDER = "data/"
 PDB_FILE = "1BRS"
@@ -7,5 +8,9 @@ PDB_FILE = "1BRS"
 if __name__ == "__main__":
     pdb_data = pdbreader.read_pdb(DATA_FOLDER + PDB_FILE + ".pdb")
     atom_df = pdb_data['ATOM']
-    print(atom_df.head())
+    atom_df_numeric = atom_df.select_dtypes(include=['number'])
+    atom_df_numeric = atom_df_numeric.drop(['model_id', 'id'], axis=1)
+    print(atom_df_numeric.head())
+    pd.plotting.parallel_coordinates(atom_df_numeric, 'resid')
+    plt.show()
 
